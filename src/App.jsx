fbe75a7d6920b07d7507efc9202bcc25e978c76b
NguyenThiThuHang0406 <hangthongminh04062004@gmail.com>
2025-03-10 17:13:39 +0700

    add base

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import Home from "./pages/home/Home";
+
+const App = () => {
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+  return <>{routes}</>;
+};
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import React from "react";
+
+const Home = () => {
+  return <div style={{color: "red", fontSize: "16px"}}>Home</div>;
+};
+
+export default Home;
