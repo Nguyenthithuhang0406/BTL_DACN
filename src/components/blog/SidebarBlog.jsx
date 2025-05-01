70bf9de38653eedd5aada285de21098e28b34311
vthn303 <vthn303@gmail.com>
2025-05-01 07:47:24 +0700

    ✨feature: add blog

---
+++ src/components/blog/SidebarBlog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SidebarBlog = () => {
+  return (
+    <div>SidebarBlog</div>
+  )
+}
+
+export default SidebarBlog
---
+++ src/pages/blog/LayoutBlog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LayoutBlog = ({children}) => {
+  return (
+    <div>LayoutBlog</div>
+  )
+}
+
+export default LayoutBlog
