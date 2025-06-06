3c6b81588b34a52cfad2808195405bb76595aa45
vthn303 <vthn303@gmail.com>
2025-06-06 12:45:59 +0700

    🐞 fix: headerAdmin

--- src/components/admin/HeaderAdmin.jsx
+++ src/components/admin/HeaderAdmin.jsx
@@ -9,7 +9,7 @@ const HeaderAdmin = ({title}) => {
 				</h1>
 				<div>
 					<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-						Logout
+						Đăng xuất
 					</button>
 				</div>
 			</div>
