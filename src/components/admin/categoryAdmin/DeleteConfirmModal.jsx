3515448d83cd1e950d1e8392f82cbe36f244425b
vthn303 <vthn303@gmail.com>
2025-06-06 12:47:45 +0700

    🐞 fix: delete modal

--- src/components/admin/categoryAdmin/DeleteConfirmModal.jsx
+++ src/components/admin/categoryAdmin/DeleteConfirmModal.jsx
@@ -4,8 +4,8 @@ const DeleteConfirmModal = ({ isOpen, onClose, onConfirm, category }) => {
 	if (!isOpen) return null;
 
 	return (
-		<div className="fixed inset-0 bg-[#0000009e] bg-opacity-50 flex items-center justify-center z-50 p-4">
-			<div className="bg-white rounded-lg shadow-lg w-full max-w-md p-6">
+		<div className="fixed inset-0 bg-[#0000009e] bg-opacity-50 flex items-center justify-center z-50 p-4 >" onClick={onClose} >
+			<div className="bg-white rounded-lg shadow-lg w-full max-w-md p-6" onClick={(e) => e.stopPropagation()}>
 				<h3 className="text-lg font-semibold mb-4">Confirm Deletion</h3>
 				<p className="text-gray-600 mb-6">
 					Xác nhận xóa danh mục{" "}
@@ -14,15 +14,15 @@ const DeleteConfirmModal = ({ isOpen, onClose, onConfirm, category }) => {
 				<div className="flex justify-end space-x-3">
 					<button
 						onClick={onClose}
-						className="px-4 py-2 border border-gray-300 rounded-lg text-gray-700 hover:bg-gray-50"
+						className="px-4 py-2 border border-gray-300 rounded-lg text-gray-700 cursor-pointer hover:bg-gray-50"
 					>
-						Cancel
+						Hủy
 					</button>
 					<button
 						onClick={onConfirm}
-						className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
+						className="px-4 py-2 bg-red-500 text-white rounded-lg cursor-pointer hover:bg-red-700 duration-200"
 					>
-						Delete
+						Xóa
 					</button>
 				</div>
 			</div>
