087fad94ff02ea71dc173230722792a51709c2d0
vthn303 <vthn303@gmail.com>
2025-06-06 12:49:49 +0700

    🐞 fix: order admin

--- src/pages/admin/OrderAdminPage.jsx
+++ src/pages/admin/OrderAdminPage.jsx
@@ -58,7 +58,7 @@ const OrderAdminPage = () => {
 	return (
 		<LayoutAdmin>
 			<div className="flex-1 overflow-auto relative z-10">
-				<HeaderAdmin title={"Orders"}/>
+				<HeaderAdmin title={"Quản lý đơn hàng"}/>
         <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
           <motion.div
            initial={{ opacity: 0, x: 30 }}
